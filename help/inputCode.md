e051fd29a0123612cde1d8402f19ae73611e98a9
ErikMac <erikyuyz@gmail.com>
2018-03-04 01:13:40 +0800

    实现input组件的双向数据绑定
    实现input组件的双向数据绑定

---
+++ help/inputCode.md
@@ -0,0 +1,7 @@
+HostListener 装饰器
+HostBinding 装饰器
+host element（宿主元素)就是应用指令的元素；
+
+@HostListener() 用于给宿主元素增加事件监听，在ng-magneto中利用这个装饰器来绑定ngModel的相关事件；
+
+NG_VALUE_ACCESSOR 用于实现自定义组件的双向数据绑定
--- src/app/components/input/ngm-input.component.ts
+++ src/app/components/input/ngm-input.component.ts
@@ -1,28 +1,36 @@
-import { Component, Input, ElementRef } from '@angular/core';
+import { Component, Input, ElementRef, HostListener, forwardRef, ViewEncapsulation } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 export type InputSize = 'sm' | 'md' | 'lg';
 
 @Component({
     selector: 'ngm-input',
+    encapsulation: ViewEncapsulation.None,
     templateUrl: './ngm-input.component.html',
-    styleUrls: ['./styles/ngm-input.component.less']
+    styleUrls: ['./styles/ngm-input.component.less'],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => NgmInputComponent),
+        multi: true
+    }],
 })
-export class NgmInputComponent {
+export class NgmInputComponent implements ControlValueAccessor {
     private _prefix = 'mag-input';
     private _el: HTMLElement;
     private _size: InputSize = 'md';
     private _classMap;
+    private _composing = false; // 正在输入
 
-    private ngmValue = '123'
+    private _value: string;
 
     @Input() ngmPlaceholder: string;
 
     @Input()
-    get ngmSize():InputSize {
+    get ngmSize(): InputSize {
         return this._size;
     }
     set ngmSize(val: InputSize) {
-        this._size = {large: 'lg', small: 'sm', middle: 'md'}[val];
+        this._size = { large: 'lg', small: 'sm', middle: 'md' }[val];
         this._setClass();
     }
 
@@ -32,7 +40,54 @@ export class NgmInputComponent {
         }
     }
 
+    // ngModel hack
+    onChange: (val: string) => void = () => null;
+    onTouched: () => void = () => null;
+
+    // 监听宿主元素的开始输入事件
+    // todo: still no usage, need bugfix
+    @HostListener('compositionstart', ['$event'])
+    compositionStart(e) {
+        console.log(e);
+        this._composing = true;
+    }
+
+    @HostListener('compositionend', ['$event'])
+    compositionEnd(e) {
+        console.log(e)
+        this._composing = false;
+        this.onChange(this._value);
+    }
+
+    // ngModel 绑定数据
+
+    get ngmValue() {
+        return this._value;
+    }
+
+    set ngmValue(val: string) {
+        if ((this._value === val) || ((this._value == null) && (val == null))) {
+            return;
+        }
+        this._value = val;
+        if (!this._composing) {
+            this.onChange(val);
+        }
+    }
+
     constructor(private elementRef: ElementRef) {
         this._el = this.elementRef.nativeElement;
+    }
+
+    writeValue(val: string) {
+        this._value = val;
+    }
+
+    registerOnChange(fn) {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: () => void): void {
+        this.onTouched = fn;
     }
 }
